6e448fdd694829da996076e306a23b708ff8fca0
pieceofshaw <71112852+pieceofshaw@users.noreply.github.com>
2023-02-06 15:35:04 +0800

    HWS with SwiftUI Day 25 Challenge 2

---
+++ Challenge2_second/Challenge2_second/Challenge2_secondApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2_secondApp.swift
+//  Challenge2_second
+//
+//  Created by Zaiyuan Xiao on 2023/2/6.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2_secondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
